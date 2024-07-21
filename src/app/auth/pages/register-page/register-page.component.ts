68f8ffcd7ac72b984934ef0a0852777dd1c96115
Sebastián Serrisuela <sebaserri@gmail.com>
2024-07-21 20:43:46 -0300

    Muevo la carpeta assets a public y comienzo a crear la estructura del form login.

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styleUrl: './register-page.component.css'
+  styleUrl: './register-page.component.css',
+  host: {'collision-id': 'RegisterPageComponent'},
 })
 export class RegisterPageComponent {
 
